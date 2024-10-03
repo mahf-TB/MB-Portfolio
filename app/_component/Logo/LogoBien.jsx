9801d7f149a71c91452548e49267b5b9d901e9ab
mahf-TB <mahefatsilavirintsoa@gmail.com>
2024-10-03 18:51:27 +0300

    image

--- app/_component/Logo/LogoBien.jsx
+++ app/_component/Logo/LogoBien.jsx
@@ -4,7 +4,8 @@ import React from "react";
 export default function LogoBien() {
   return (
     <span className="text-green4 flex gap-2">
-      <Image src={"/image/logo.png"} alt="logo" width={20} height={20} />
+      {/* <img src="/image/Logo.png" alt="logo" className="w-[5px] object-cover h-[5px]" /> */}
+      <Image src={"/image/Logo.png"} alt="Logo" width={20} height={20} />
       Bienvenu
     </span>
   );
